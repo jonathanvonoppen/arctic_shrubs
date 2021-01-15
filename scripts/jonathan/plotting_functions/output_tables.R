a8fa8b10aacf3c75be5df23aa19ca6e59efbe35f
Jonathan Von Oppen <au630524@uni.au.dk>
2021-01-15 18:07:55 +0100

    compiled SI tables

--- scripts/jonathan/plotting_functions/output_tables.R
+++ scripts/jonathan/plotting_functions/output_tables.R
@@ -7,7 +7,8 @@
 # Jonathan von Oppen, Aarhus University, Jan 2021 - jonathan.vonoppen [at] bio.au.dk
 
 
-output_table <- function(species){
+# species ----
+output_table_sp <- function(species){
   
   # define output data
   if(species == "Betula nana") model_coeff_output <- coeff.shrub_gradient.BetNan2
@@ -58,6 +59,9 @@ output_table <- function(species){
     
     # set simple b/w theme w/ right-aligned num cols
     theme_vanilla() %>% 
+    # define NA string
+    # set_formatter_type(na_str = "--") %>% 
+    #colformat_num(na_str = "--") %>% 
     # set digits for all cols to 2 and for Rhat col to 3
     set_formatter(mean = function(x) sprintf("%.02f", x),
                   sd = function(x) sprintf("%.02f", x),
@@ -67,10 +71,84 @@ output_table <- function(species){
                   u95 = function(x) sprintf("%.02f", x),
                   Rhat = function(x) sprintf("%.03f", x)) %>% 
     # define width of table
-    #set_table_properties(width = .8, layout = "autofit") %>% 
+    set_table_properties(width = .8, layout = "autofit") %>% 
     # remove inner borders
     border_inner_h(border = officer::fp_border(color = "white")) %>% 
     # set padding for better readability
-    padding(padding = 6, part = "all")
+    padding(padding = 6, part = "all") %>% 
+    # adjust font size
+    fontsize(size = 10, part = "all") #%>% 
+  
+    # set caption
+    #set_caption("")
+  
+}
+
+# functional groups ----
+output_table_gr <- function(fgroup){
+  
+  # define output data
+  if(fgroup == "all shrubs") model_coeff_output <- coeff.shrub_gradient.AllShr2
+  if(fgroup == "evergreen shrubs") model_coeff_output <- coeff.shrub_gradient.AllEve2
+  if(fgroup == "deciduous shrubs") model_coeff_output <- coeff.shrub_gradient.AllDec2
+  
+  # set row name order
+  var_order <- c("intercept", 
+                 "b.tempjja.x", "b.tempjja.x2", "b.tempcont.x", "b.tempcont.x2", "b.precipjja.x", "b.precipjja.x2", 
+                 "b.sri", "b.tri", "b.twi", 
+                 "b.gramin_cov", 
+                 "phi", "sigma.plotgroup")
+  
+  # wrangle output table
+  model_coeff_output %>% 
+    
+    # filter for coefficients of interest
+    filter(param %in% var_order) %>% 
+    
+    # reorder rows
+    mutate(param = factor(param, levels = var_order)) %>% 
+    arrange(param) %>%
+    mutate(param = as.character(param)) %>% 
+    
+    # adjust parameter namings
+    mutate(param = case_when(param == "b.gramin_cov" ~ "graminoid cover",
+                             param == "b.precipjja.x" ~ "summer precipitation",
+                             param == "b.precipjja.x2" ~ "summer precipitation ^2",
+                             param == "b.sri" ~ "solar radiation",
+                             param == "b.tempcont.x" ~ "temperature variability",
+                             param == "b.tempcont.x2" ~ "temperature variability ^2",
+                             param == "b.tempjja.x" ~ "summer temperature",
+                             param == "b.tempjja.x2" ~ "summer temperature ^2",
+                             param == "b.tri" ~ "terrain ruggedness",
+                             param == "b.twi" ~ "topographic wetness",
+                             TRUE ~ param)) %>% 
+    
+    # build flextable
+    flextable() %>% 
+    
+    # set simple b/w theme w/ right-aligned num cols
+    theme_vanilla() %>% 
+    # define NA string
+    # set_formatter_type(na_str = "--") %>% 
+    #colformat_num(na_str = "--") %>% 
+    # set digits for all cols to 2 and for Rhat col to 3
+    set_formatter(mean = function(x) sprintf("%.02f", x),
+                  sd = function(x) sprintf("%.02f", x),
+                  l95 = function(x) sprintf("%.02f", x),
+                  l90 = function(x) sprintf("%.02f", x),
+                  u90 = function(x) sprintf("%.02f", x),
+                  u95 = function(x) sprintf("%.02f", x),
+                  Rhat = function(x) sprintf("%.03f", x)) %>% 
+    # define width of table
+    set_table_properties(width = .8, layout = "autofit") %>% 
+    # remove inner borders
+    border_inner_h(border = officer::fp_border(color = "white")) %>% 
+    # set padding for better readability
+    padding(padding = 6, part = "all") %>% 
+    # adjust font size
+    fontsize(size = 10, part = "all") #%>% 
+  
+  # set caption
+  #set_caption("")
   
 }
